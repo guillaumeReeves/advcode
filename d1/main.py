7c421d9685876fad2f3dd8388f113315c04efecc
Guillaume Reeves <greeves@bitfarms.com>
2024-12-12 23:39:05 +0000

    First day

---
+++ d1/main.py
@@ -0,0 +1,24 @@
+#!/bin/python
+
+file = "numbers.txt"
+
+d1 = []
+d2 = []
+with open(file) as f:
+    for line in f:
+        ls = line.split()
+        d1.append(int(ls[0]))
+        d2.append(int(ls[1]))
+
+d1.sort()
+d2.sort()
+
+d1c = d1
+d2c = d2
+
+total = 0;
+for i in range(len(d1)):
+    diff = abs(d1[i] -d2[i])
+    total += diff
+
+print(total)
